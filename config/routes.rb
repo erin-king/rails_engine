ddce317b5d6f3a7be2ddf25c37c4a0c310148289
Erin King <44042415+erin-king@users.noreply.github.com>
2019-04-30 19:56:01 -0600

    Add invoices_controller#index, merchant endpoint for merchants/:id/invoices, route

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,8 @@ Rails.application.routes.draw do
   namespace :api do
     namespace :v1 do
       resources :merchants, only: [:index, :show] do
-        resources :items
+        resources :items, only: [:index]
+        resources :invoices, only: [:index]
       end
     end
   end
--- spec/requests/api/v1/merchants_request_spec.rb
+++ spec/requests/api/v1/merchants_request_spec.rb
@@ -12,9 +12,25 @@ describe "Merchants API" do
 
     expect(response).to be_successful
 
-    merchant = JSON.parse(response.body)
+    items = JSON.parse(response.body)
 
-    expect(m1.items.count).to eq(3)
+    expect(items.count).to eq(3)
   end
 
+  it "returns a collection of invoices associated with a merchant from their known orders" do
+
+    m1 = create(:merchant)
+    c1 = create(:customer)
+    i1 = create(:invoice, merchant_id: m1.id, customer_id: c1.id)
+    i2 = create(:invoice, merchant_id: m1.id, customer_id: c1.id)
+    i3 = create(:invoice, merchant_id: m1.id, customer_id: c1.id)
+
+    get "/api/v1/merchants/#{m1.id}/invoices"
+
+    expect(response).to be_successful
+
+    invoices = JSON.parse(response.body)
+
+    expect(invoices.count).to eq(3)
+  end
 end
