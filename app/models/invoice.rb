7e075d66fa08fd827c0cf0137bad6ef635b8be5e
Erin King <44042415+erin-king@users.noreply.github.com>
2019-05-01 19:05:12 -0600

    Add tests for and validations and relationships to invoice and invoice_item models

--- app/models/invoice.rb
+++ app/models/invoice.rb
@@ -1,7 +1,11 @@
 class Invoice < ApplicationRecord
-  belongs_to :customer
-  belongs_to :merchant
+
+  validates_presence_of :status
+
   has_many :transactions
   has_many :invoice_items
   has_many :items, through: :invoice_items
+  belongs_to :customer
+  belongs_to :merchant
+
 end
