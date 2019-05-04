6d88f7c8f26c703a93a2c4f1756b7e5bc5519c59
Erin King <44042415+erin-king@users.noreply.github.com>
2019-05-04 14:26:19 -0600

    Adjust merchants#find_all test to include a merchant without matching search parameters

--- spec/requests/api/v1/merchants_request_spec.rb
+++ spec/requests/api/v1/merchants_request_spec.rb
@@ -69,18 +69,19 @@ describe "Merchants API" do
   end
 
   it "returns an array of merchants by find_all parameters" do
-    m1 = create(:merchant)
-    m2 = create(:merchant)
+    m1 = create(:merchant, name: "Erin")
+    m2 = create(:merchant, name: "Erin")
+    m3 = create(:merchant, name: "Ben")
 
-    get "/api/v1/merchants/find?id=#{m1.id}"
+    get "/api/v1/merchants/find_all?name=Erin"
 
     expect(response).to be_successful
     merchants = JSON.parse(response.body)
     merchant = merchants["data"]
-    expect(merchant["id"]).to eq("#{m1.id}")
-    expect(merchant["id"]).to_not eq("#{m2.id}")
 
-    expect(merchants.count).to eq(1)
+    expect(merchant[0]["attributes"]["name"]).to eq("#{m1.name}")
+    expect(merchant[1]["attributes"]["name"]).to eq("#{m2.name}")
+    expect(merchant[2]).to eq(nil)
   end
 
   it "returns a collection of items associated with that merchant" do
